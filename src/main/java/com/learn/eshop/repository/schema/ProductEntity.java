4e6e673b26dbe0fe1de09de72c15a47c6cd3d0cc
mostafijurr <Mostafijur.Rahman@brainstation23.com>
2021-04-27 12:18:57 +0600

    add: product entity

--- src/main/java/com/learn/eshop/repository/schema/ProductEntity.java
+++ src/main/java/com/learn/eshop/repository/schema/ProductEntity.java
@@ -1,6 +1,5 @@
 package com.learn.eshop.repository.schema;
 
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -10,20 +9,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Accessors(chain = true)
 @Entity
-@Table(name = SchemaConstant.PRODUCT_QUANTITY_TABLE_NAME)
-public class ProductCategoryEntity {
+@Table(name = SchemaConstant.PRODUCT_TABLE_NAME)
+public class ProductEntity {
 
   @Id
-  @Type(type = "uuid-char")
   @GeneratedValue
+  @Type(type = "uuid-char")
   private UUID id;
 
-  private String categoryName;
+  private String name;
+
+  private String sku;
+
+  private String description;
+
+  private String imageUrl;
+
+  private BigDecimal unitPrice;
+
+  private int unitInStock;
+
+  private boolean active;
+
+  private Date dateCreated;
+
+  private Date lastUpdated;
 
 }
--- src/main/java/com/learn/eshop/repository/schema/SchemaConstant.java
+++ src/main/java/com/learn/eshop/repository/schema/SchemaConstant.java
@@ -2,4 +2,5 @@ package com.learn.eshop.repository.schema;
 
 public class SchemaConstant {
   static final String PRODUCT_QUANTITY_TABLE_NAME = "Product_Quantity";
+  static final String PRODUCT_TABLE_NAME = "Product";
 }
